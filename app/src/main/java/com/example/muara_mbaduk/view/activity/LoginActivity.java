7fdfb571347e95f6084fc0f656a54a71d690bb7e
slametananas <kemalslamet29@gmail.com>
2023-04-11 20:42:07 +0700

    first commit

--- app/src/main/java/com/example/muara_mbaduk/view/activity/LoginActivity.java
+++ app/src/main/java/com/example/muara_mbaduk/view/activity/LoginActivity.java
@@ -2,15 +2,26 @@ package com.example.muara_mbaduk.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.muara_mbaduk.R;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        login = findViewById(R.id.loginid);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
