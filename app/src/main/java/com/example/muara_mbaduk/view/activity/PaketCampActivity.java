bae49037b0950e7d000893117b79ef39c52ba2bf
slametananas <kemalslamet29@gmail.com>
2023-04-24 12:14:38 +0700

    harga tiket done

---
+++ app/src/main/java/com/example/muara_mbaduk/view/activity/PaketCampActivity.java
@@ -0,0 +1,16 @@
+package com.example.muara_mbaduk.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.muara_mbaduk.R;
+
+public class PaketCampActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paket_camp);
+    }
+}
