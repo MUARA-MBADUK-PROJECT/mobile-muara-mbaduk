6783c478c7d9bef587fd7a432d21346147c95201
slametananas <kemalslamet29@gmail.com>
2023-05-08 08:38:07 +0700

    layout

--- app/src/main/java/com/example/muara_mbaduk/view/activity/PaketCampActivity.java
+++ app/src/main/java/com/example/muara_mbaduk/view/activity/PaketCampActivity.java
@@ -1,16 +1,34 @@
 package com.example.muara_mbaduk.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.example.muara_mbaduk.R;
 
-public class PaketCampActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class PaketCampActivity extends AppCompatActivity {
+ArrayList<PaketCampModel> paketCampModels = new ArrayList<>();
+int[] paketGambar = {R.drawable.baseline_image_24_green, R.drawable.baseline_image_24_yellow, R.drawable.baseline_image_24_red, R.drawable.baseline_image_24};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paket_camp);
+        RecyclerView recyclerView = findViewById(R.id.paketCampRecyclerView);
+        SetUpPaketCampModels();
+        PaketCamp_RecyclerViewAdapter adapter = new PaketCamp_RecyclerViewAdapter(this, paketCampModels);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+    private void SetUpPaketCampModels(){
+        String[] paketNama = getResources().getStringArray(R.array.nama_paket_camp_txt);
+        String[] paketHarga = getResources().getStringArray(R.array.paket_harga_txt);
+
+        for (int i = 0; i<paketNama.length; i++) {
+            paketCampModels.add(new PaketCampModel(paketNama[i], paketHarga[i], paketGambar[i]));
+        }
     }
 }
